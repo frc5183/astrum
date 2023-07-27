6f25c2cb484f08ed4a9cd19207582bd20bc9b68a
Trip-kun <kaidenmcmartin1@gmail.com>
2023-07-27 19:46:04 -0400

    state.lua REWRITE and Documenatation

--- state.lua
+++ state.lua
@@ -1,39 +1,36 @@
-local log = require("lib.log")
+-- Imports
+local log = require"lib.log"
+local safety = require"lib.safety"
 local state={}
 local active={name="init"}
-
+--- A function that allows quick switching between different functions for the Love2D callbacks like love.draw() and love.update().
+-- A table is passed in containing all replacement callbacks. In addition to Love2D callbacks, two more are available:
+-- the callback switchaway() is used after the state.switch() is called again to allow cleaning up if needed to fully disable the state.
+-- the callback switchto() is the complement to switchaway() it is called immediately when state.switch() is called and allows setup that may be needed to enable the state again
+-- @param newState a table that contains all replacment callbacks
 function state.switch(newState)
-	if type(newState)~="table" then
-		log.error("Value newState expected to be of type table. Found type " .. type(newState).. ".")
-		error("Value newState expected to be of type table. Found type " .. type(newState).. ".")
-	end
+  safety.ensureTable(newState, "newState")
+  if (not (type(name) == "nil")) then
+    safety.ensureString(name, "newState.name")
+  end
 	local name=newState.name
-	if not (type(name)=="string" or type(name)=="nil") then
-		log.error("Value newState.name expected to be of type string or nil. Found type " .. type(name).. ".")
-		error("Value newState.name expected to be of type string or nil. Found type " .. type(name).. ".")
-	end
+	if (not (type(name) == "nil")) then
+    safety.ensureString(name, "newState.name")
+  end
 	if (not name or name=="") then
 		log.warn("Switching to a state without a name. Defaulting to name NONAME")
 		name="NONAME"
 		newState.name=name
 	end
 	log.info("Switching to a new gamestate with name: " .. name)
-	
-	if type(active.switchaway)~="nil" then
-		if type(active.switchaway) ~= "function" then
-			log.error("Value active.switchaway expected to be of type nil or function. Found type " .. type(active.switchaway) .. ". The current call to state.switch did not cause this issue. Check the last call before this one.")
-			error("Value active.switchaway expected to be of type nil or function. Found type " .. type(active.switchaway).. ". The current call to state.switch did not cause this issue. Check the last call before this one.")
-		end
-		active.switchaway()
-	end
-	
-	if type(newState.switchto)~="nil" then
-		if type(newState.switchto)~="function" then
-			log.error("Value newState.switchto expected to be of type nil or function. Found type " .. type(newState.switchto).. ".")
-			error("Value newState.switchto expected to be of type nil or function. Found type " .. type(newState.switchto).. ".")
-		end
+	if (not (type(active.switchaway) == "nil")) then
+    safety.ensureFunction(active.switchaway, "active.switchaway: THIS INDICATES THE LAST STATE IS THE PROBLEM. NOT THE ONE THAT IS BEING SWITCHED TO")
+    active.switchaway()
+  end
+	if (not (type(newState.switchto)=="nil")) then
+    safety.ensureFunction(newState.switchto, "newState.switchto")
     newState.switchto()
-	end
+  end
 	
 	for k, v in pairs(newState) do
 		if k=="content" then
@@ -46,8 +43,10 @@ function state.switch(newState)
 	end
 	active=newState
 end
+--- A function that gets the name of the currently active state.
+-- @return the name of the currently active state
 function state.getStateName()
 	return active.name
 end
-log.info("Gamestate Library Loaded.")
+-- Return
 return state
