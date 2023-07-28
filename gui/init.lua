29ae36eaee39203b77a1875a951f6570ffd440ab
Trip-kun <kaidenmcmartin1@gmail.com>
2023-07-28 00:26:51 -0400

    Replace ClickPulser with ClickNode This is a BREAKING CHANGE and the library is not finished updating and is not currently usable (for the GUI module anyways) Completes #1

--- gui/init.lua
+++ gui/init.lua
@@ -1,8 +1,10 @@
+local ClickNode = require"lib.gui.subsystem.ClickNode"
 return {
   AdapterButton=require"lib.gui.element.AdapterButton",
   TextButton=require"lib.gui.element.TextButton",
   VisualButton=require"lib.gui.element.VisualButton",
-  ClickPulser=require"lib.gui.subsystem.ClickPulser",
+  ClickOrigin=ClickNode(true, function(a, b, c) return a, b, c end),
+  ClickNode = ClickNode,
   Color=require"lib.gui.color",
   TextInput=require"lib.gui.element.TextInput",
   ScrollBar=require"lib.gui.element.ScrollBar"
---
+++ gui/subsystem/ClickNode.lua
@@ -0,0 +1,42 @@
+-- Imports
+local pulse = require"lib.pulse"
+local math2 = require"lib.math2"
+local safety = require"lib.safety"
+local ClickPulser = pulse({"onClick", "onPress"})
+local count = 0
+
+--- Returns a new ClickNode
+-- If Origin is True, then it creates a top level node, otherwise it creates a child node of the ClickNode that Origin is
+return function (origin, adapter)
+  safety.ensureFunction(adapter, "adapter")
+  local o = pulse({"onClick", "onPress"})
+  if origin!=true then
+    safety.ensureInstanceType(origin, pulse, "origin")
+    origin:onEvent("onPress", "ClickNode" .. count+1, 
+    function (pt, button, presses, ...) 
+      pt, button, presses = adapter(pt, button, presses)
+      o:emit("onPress", pt, button, presses, ...)
+    end)
+    origin:onEvent("onClick", "ClickNode" .. count+1,
+    function (pt, button, presses, ...)
+      pt, button, presses = adapter(pt, button, presses)
+      o:emit("onClick", pt, button, presses, ...)
+    count=count+1
+  else
+    function o:mouseReleased(x, y, button, presses)
+      safety.ensureNumber(x, "x")
+      safety.ensureNumber(y, "y")
+      safety.ensureNumber(button, "button")
+      safety.ensureNumber(presses, "presses")
+      self:emit("onClick", math2.Point2D(x, y), button, presses)
+    end
+
+    function o:mousePressed(x, y, button, presses)
+      safety.ensureNumber(x, "x")
+      safety.ensureNumber(y, "y")
+      safety.ensureNumber(button, "button")
+      safety.ensureNumber(presses, "presses")
+      self:emit("onPress", math2.Point2D(x, y), button, presses)
+    end
+  end
+end
