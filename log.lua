d5b571e414409ac26e07adad3e18765e2a9f2d09
Trip-kun <kaidenmcmartin1@gmail.com>
2023-07-27 22:32:04 -0400

    Document log.lua (it's so short the docs probably doubled the size)

--- log.lua
+++ log.lua
@@ -1,26 +1,34 @@
+-- Imports
+local log = {}
+--- Prints log messages to the console
+-- @param level the custom log type name
+-- @param msg the log message to be printed to the console
 local function Log(level, msg)
 	print("[" .. level .. "] " .. msg)
 end
 
 
-local log = {}
-
+--- Prints warning log messages to the console
+-- @param msg the log message to be printed to the console
 function log.warn(msg)
 	Log("WARN", msg)
 end
-
+--- Prints info log messages to the console
+-- @param msg the log message to be printed to the console
 function log.info(msg)
 	Log("INFO", msg)
 end
-
+--- Prints error log messages to the console
+-- @param msg the log message to be printed to the console
 function log.error(msg)
 	Log("ERROR", msg)
 end
-
+--- Prints fatal log messages to the console
+-- @param msg the log message to be printed to the console
 function log.fatal(msg)
 	Log("FATAL", msg)
 end
-
+-- Return
 log.custom=Log
 log.info("Console Logging Library Loaded.")
 return log
