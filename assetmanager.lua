709af2ed674d2650c4e786d2aa3b4aa6ff66a69b
Trip-kun <kaidenmcmartin1@gmail.com>
2023-07-27 22:56:35 -0400

    Document assetmanager.lua

--- assetmanager.lua
+++ assetmanager.lua
@@ -1,8 +1,15 @@
+-- Imports
 local safety = require"lib.safety"
 local assets = {}
 local asset_manager = {}
 local class = require"lib.external.class"
 local Asset = class("Asset")
+--- An Asset to be returned by AssetManager
+-- The Idea is that Assets can be serialized while maintaining their link to their asset contents when reloaded
+-- the Asset.data may also be used to persist data in an instance of Asset through serialization. 
+-- @param category the string category where the asset is contained within the assetmanager
+-- @param id the string id within the category where the asset is found
+-- @returnt the asset object
 function Asset:initialize(category, id)
   safety.ensureString(category, "category")
   safety.ensureString(id, "id")
@@ -10,12 +17,18 @@ function Asset:initialize(category, id)
   self.id=id
   self.data={}
 end
+--- Returns the actual digital asset from the asset_manager
+-- @return the actual asset
 function Asset:getAsset()
   return assets[self.category][self.id]
 end
+--- Declassifies the Asset, useful for serialization
+-- @return the declassified asset
 function Asset:export()
   return {category=self.category, id=self.id, data=self.data}
 end
+--- Registers a category within the asset_manager, with strict duplicate checking
+-- @param the string category to register
 function asset_manager.registerCategory(category)
   safety.ensureString(category, "category")
   if (assets[category]~=nil) then 
@@ -23,7 +36,10 @@ function asset_manager.registerCategory(category)
   end
   assets[category]={}
 end
-
+--- Registers an asset within a registered category, with strict duplicate and required registerd category
+-- @param category the REGISTERED category
+-- @param id the id to register
+-- @param asset the asset to which the category+id refers
 function asset_manager.registerAsset(category, id, asset)
   safety.ensureString(category, "category")
   safety.ensureString(id, "id")
@@ -38,7 +54,9 @@ function asset_manager.registerAsset(category, id, asset)
   end
   assets[category][id]=asset
 end
-
+--- Removes an asset within a registered category, with strict duplicate and required registerd category
+-- @param category the REGISTERED category
+-- @param id the id to register
 function asset_manager.removeAsset(category, id)
   safety.ensureString(category, "category")
   safety.ensureString(id, "id")
@@ -50,6 +68,10 @@ function asset_manager.removeAsset(category, id)
   end
   assets[category][id]=nil
 end
+--- Fetches an asset with the category and id, with strict registration requirements
+-- @param category the REGISTERED category
+-- @param id the REGISTERED id-asset
+-- @return a NEW asset instance for that registered asset
 function asset_manager.fetchAsset(category, id)
   safety.ensureString(category, "category")
   safety.ensureString(id, "id")
@@ -61,7 +83,12 @@ function asset_manager.fetchAsset(category, id)
   end
   return Asset(category, id)
 end
-
+--- Assembles an asset with existing data, and a registerd category and id
+-- Useful for Deserialization
+-- @param category the REGISTERED category
+-- @param id the REGISTERED id-asset
+-- @param data the nullable data to assemble the asset with
+-- @return the new reassembled Asset instance
 function asset_manager.assembleAsset(category, id, data)
   safety.ensureString(category, "category")
   safety.ensureString(id, "id")
@@ -78,5 +105,8 @@ function asset_manager.assembleAsset(category, id, data)
   out.data=data or {}
   return out
 end
+--- Returns the Asset class for rare needs for it 
+-- @return the Asset class
 function asset_manager.getAssetClass() return Asset end
+-- Return
 return asset_manager
