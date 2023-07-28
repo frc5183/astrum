96c515ca251964e9e10d88f05f646cdca679b701
Trip-kun <kaidenmcmartin1@gmail.com>
2023-07-27 23:12:01 -0400

    Document assetloader.lua

--- assetloader.lua
+++ assetloader.lua
@@ -1,3 +1,4 @@
+-- Imports
 local AssetLoader = {}
 local log = require"lib.log"
 local AssetManager = require"lib.assetmanager"
@@ -6,6 +7,9 @@ love.filesystem.setIdentity(love.filesystem.getIdentity(), true)
 local int_asset_scripts = love.filesystem.getDirectoryItems("asset/script")
 local threads = {}
 local isFinished=false
+--- loads files from a directory
+-- @param dir the string for the directory to be searched
+-- @param script_type the script type. if Local, will only load files within the source code package or directory
 local function load_dir(dir, script_type)
   local asset_scripts
   asset_scripts = love.filesystem.getDirectoryItems(dir)
@@ -35,17 +39,18 @@ local function load_dir(dir, script_type)
   end
 end
 local external = false
+--- Enables external scripts
 function AssetLoader.enableExternal()
   external=true
 end
-  
+--- Starts loading scripts
 function AssetLoader.start()
   load_dir("asset/script", "Local")
   if (external) then
     load_dir(love.filesystem.getSaveDirectory() .. "/external/asset/script", "External")
   end
 end
-
+--- Manages running scripts and receives incoming assets 
 function AssetLoader.update()
   
   local rm = {}
@@ -74,7 +79,21 @@ function AssetLoader.update()
     isFinished=true
   end
 end
+--- Returns if all scripts are finished running
+-- @return isFinished
 function AssetLoader.isFinished()
   return isFinished
 end
+--[[
+IMPORTANT NOTES:
+ASSET SCRIPTS SHOULD RETURN ASSETS TO THE CHANNEL "assets"
+ASSETS SHOULD BE RETURNED IN THE FORM {category=category, id=id, asset=asset}
+EXTERNAL ASSET SCRIPTS ARE RISKY AND NOT RECOMMENDED AS ANY CODE COULD BE COLLECTED AND RUN, INCLUDING MALICIOUS CODE
+SCRIPTS ARE RUN AS THREADS AND THEREFORE MAY NEED TO RELOAD CERTAIN REQUIRED LOVE2D OR ASTRUM LIBRARIES
+LOCAL SCRIPTS SHOULD BE INCLUDED IN THE src/asset/script/ DIRECTORY
+EXTERNAL SCRIPTS SHOULD BE INCLUDED IN THE LOVESAVEDIRECTORY/external/asset/script/ DIRECTORY
+ANY DATA SENT AS AN ASSET CANNOT INCLUDE FUNCTIONS, COROUTINES OR NON LOVE2D-USERDATA
+CLASS INSTANCES SHOULD BE DECLASSIFIED IF LOADING THEM AS AN ASSET IS NECESSARY
+--]]
+-- Return
 return AssetLoader
