81f3b87a4a9ed57ef4b7f1b3004dc7b8f402f067
Trip-kun <kaidenmcmartin1@gmail.com>
2023-07-27 20:42:23 -0400

    Document pulse.lua

--- pulse.lua
+++ pulse.lua
@@ -1,7 +1,10 @@
 local class = require"lib.external.class"
 local Pulse = class("Pulse")
 local safety = require"lib.safety"
-
+--- A Pulse is an event emitter class. Instances can be used to construct complex callback-based programming
+-- @type Pulse
+-- @param events a nullable table that contains strings to be initialized as events
+-- @return a new Pulse instance
 function Pulse:initialize(events)
   if not (type(events)=="table" or type(events)=="nil") then
     safety.ensureTable(events, "events")
@@ -14,16 +17,22 @@ function Pulse:initialize(events)
     self.events[v]={}
   end
 end
-
+--- Registers a new event for the Pulse
+-- @param event the string name for the event
 function Pulse:newEvent(event)
   safety.ensureString(event, "event")
+  self.events[event]={}
 end
-
+--- Unregisters an event for the Pulse
+-- @param event the string name for the event
 function Pulse:removeEvent(event)
   safety.ensureString(event, "event")
   self.events[event]=nil
 end
-
+--- Registers (or replaces) a new callback for the Pulse
+-- @param event the string name for the event: MUST BE REGISTERED ALREADY
+-- @param id the string id for the callback: Must be unique if not trying to replace other callbacks
+-- @param func the callback function itself
 function Pulse:onEvent(event, id, func)
   safety.ensureString(event, "event")
   safety.ensureString(id, "id")
@@ -33,7 +42,9 @@ function Pulse:onEvent(event, id, func)
   end
   self.events[event][id]=func
 end
-
+--- Removes a callback, whether it exists or not, for the Pulse 
+-- @param event the string name for the event: MUST BE REGISTERED ALREADY
+-- @param id the id for the callback
 function Pulse:removeOnEvent(event, id)
   safety.ensureString(event, "event")
   safety.ensureString(id, "id")
@@ -43,7 +54,9 @@ function Pulse:removeOnEvent(event, id)
   end
   
 end
-
+--- Emits an event with arguments to all registered callbacks for that event
+-- @param event the string name for the event: MUST BE REGISTERED ALREADY
+-- @param ... any additional paramaters will be passed to callback functions
 function Pulse:emit(event, ...)
   safety.ensureString(event, "event")
   if (type(self.events[event]~="table")) then
@@ -58,7 +71,10 @@ function Pulse:emit(event, ...)
   end
   self.cache[event]=nil
 end
-
+--- Used by callbacks to ensure that any other callbacks that must be run before them are completed before their execution
+-- @param event the string name for the event: MUST BE REGISTERED ALREADY
+-- @param id the id for the required callback
+-- @param ... the extra paramaters to be passed to the callback
 function Pulse:require(event, id, ...)
   safety.ensureString(event, "event")
   safety.ensureString(id, "id")
@@ -72,5 +88,5 @@ function Pulse:require(event, id, ...)
     self.cache[event][id]=true
   end
 end
-
+-- Return
 return Pulse
