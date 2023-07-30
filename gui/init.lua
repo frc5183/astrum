8a08f5f91cbbbfbda663575aebb6e51f2717ceab
Trip-kun <kaidenmcmartin1@gmail.com>
2023-07-29 22:37:52 -0400

    Add ClickOrigin Document Button.lua Adapt Button.lua to ClickNode/ClickOrigin

--- gui/init.lua
+++ gui/init.lua
@@ -1,10 +1,10 @@
-local ClickNode = require"lib.gui.subsystem.ClickNode"
+
 return {
   AdapterButton=require"lib.gui.element.AdapterButton",
   TextButton=require"lib.gui.element.TextButton",
   VisualButton=require"lib.gui.element.VisualButton",
-  ClickOrigin=ClickNode(true, function(a, b, c) return a, b, c end),
-  ClickNode = ClickNode,
+  ClickOrigin=require"lib.gui.subsystem.ClickOrigin",
+  ClickNode = require"lib.gui.subsystem.ClickNode",
   Color=require"lib.gui.color",
   TextInput=require"lib.gui.element.TextInput",
   ScrollBar=require"lib.gui.element.ScrollBar"
--- gui/mixin/Button.lua
+++ gui/mixin/Button.lua
@@ -1,12 +1,25 @@
+-- Imports
 local button = {}
 local count=0;
-local ClickPulser=require"lib.gui.subsystem.ClickPulser"
+local ClickOrigin=require"lib.gui.subsystem.ClickOrigin"
 local safety=require"lib.safety"
-function button:initButton(x, y, width, height)
+--- Internal Init function for buttons
+-- @param x the x position
+-- @param y the y position
+-- @param width the width
+-- @param height the height
+-- @param node the ClickNode used for receiving events
+function button:initButton(x, y, width, height, node)
   safety.ensureNumber(x, "x")
   safety.ensureNumber(y, "y")
   safety.ensureNumber(width, "width")
   safety.ensureNumber(height, "height")
+  if (node==nil) then
+    node=ClickOrigin
+  end
+  
+  safety.ensurePulse(node, "node: SHOULD BE A CLICK NODE")
+  self.node=node
   self.count=0
   self.x=x
   self.y=y
@@ -15,12 +28,14 @@ function button:initButton(x, y, width, height)
   count=count+1
   self.id=count
 end
-
+--- Registers a function callback to the ClickNode for Click (mouse release)
+-- @param func the callback function
+-- @return the id of the callback, for later optional removal
 function button:onClick(func)
   safety.ensureFunction(func)
   local c = self.count+1
   self.count=self.count+1
-  ClickPulser:onEvent("onClick", "Button(".. tostring(self.id) .. ") Callback(" .. tostring(c) .. ")", 
+  self.node:onEvent("onClick", "Button(".. tostring(self.id) .. ") Callback(" .. tostring(c) .. ")", 
     function (pt, button, presses, ...) 
       if (self.adapter~=nil) then
         safety.ensureFunction(self.adapter, "self.adapter must be a function or nil")
@@ -31,18 +46,21 @@ function button:onClick(func)
     end)
   return c
 end
-
+--- Removes a callback function based on it's id (mouse release)
+-- @param id the callback id
 function button:removeOnClick(id)
   safety.ensureNumber(id, "id")
   local c = id
-  ClickPulser:removeOnEvent("onClick", "Button(".. tostring(self.id) .. ") Callback(" .. tostring(c) .. ")")
+  self.node:removeOnEvent("onClick", "Button(".. tostring(self.id) .. ") Callback(" .. tostring(c) .. ")")
 end
-
+--- Registers a function callback to the ClickNode for Click (mouse press)
+-- @param func the callback function
+-- @return the id of the callback, for later optional removal
 function button:onPress(func)
   safety.ensureFunction(func)
   local c = self.count+1
   self.count=self.count+1
-  ClickPulser:onEvent("onPress", "Button(".. tostring(self.id) .. ") Callback(" .. tostring(c) .. ")", 
+  self.node:onEvent("onPress", "Button(".. tostring(self.id) .. ") Callback(" .. tostring(c) .. ")", 
     function (pt, button, presses, ...) 
       if (self.adapter~=nil) then
         safety.ensureFunction(self.adapter, "self.adapter must be a function or nil")
@@ -53,29 +71,35 @@ function button:onPress(func)
     end)
   return c
 end
-
+--- Removes a callback function based on it's id (mouse press)
+-- @param id the callback id
 function button:removeOnPress(id)
   safety.ensureNumber(id, "id")
   local c = id
-  ClickPulser:removeOnEvent("onPress", "Button(".. tostring(self.id) .. ") Callback(" .. tostring(c) .. ")")
+  self.node:removeOnEvent("onPress", "Button(".. tostring(self.id) .. ") Callback(" .. tostring(c) .. ")")
 end
-
+--- A forwarder for pulse:require() (mouse release)
+-- @param id the id of the callback to require
+-- @param .. all extend ids
 function button:requireClick(id, ...)
   safety.ensureNumber(id, "id")
   local c = id
-  ClickPulser:require("onClick","Button(".. tostring(self.id) .. ") Callback(" .. tostring(c) .. ")", ...)
+  self.node:require("onClick","Button(".. tostring(self.id) .. ") Callback(" .. tostring(c) .. ")", ...)
 end
-
+--- A forwarder for pulse:require() (mouse press)
+-- @param id the id of the callback to require
+-- @param .. all extend ids
 function button:requirePress(id, ...)
   safety.ensureNumber(id, "id")
   local c = id
-  ClickPulser:require("onPress","Button(".. tostring(self.id) .. ") Callback(" .. tostring(c) .. ")", ...)
+  self.node:require("onPress","Button(".. tostring(self.id) .. ") Callback(" .. tostring(c) .. ")", ...)
 end
-
+--- A useful function for callbacks to test if a point is within the button's bounds
+-- @param point the Point2D point
 function button:contains(point) 
   safety.ensurePoint2D(point, "point")
   return point.x>=self.x and point.x<=self.x+self.width and point.y>=self.y and point.y<=self.y+self.height
 end
 
-
+-- Return
 return button
